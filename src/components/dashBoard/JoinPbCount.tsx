4fdc05fd536ddcd28061c6a72f07d603839dad14
LeeByoungWook <lbw3973@naver.com>
2023-06-27 22:30:01 +0900

    Feat: 승인 대기 PB 수 컴포넌트 구현

---
+++ src/components/dashBoard/JoinPbCount.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function JoinPbCount({ pbCount }: { pbCount: number }) {
+  return (
+    <div className="h-[150px] w-[300px] rounded-md bg-white px-5 py-4 shadow-md">
+      <p className="text-xl font-bold">승인 대기 PB 수</p>
+      <div className="text-center text-5xl font-bold">{pbCount}</div>
+    </div>
+  );
+}
+
+export default JoinPbCount;
