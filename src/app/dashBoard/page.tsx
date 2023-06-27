dabc582856897b42e81517d9a7f433818290aa33
LeeByoungWook <lbw3973@naver.com>
2023-06-27 22:30:21 +0900

    Feat: dashBoard 페이지 제작

--- src/app/dashBoard/page.tsx
+++ src/app/dashBoard/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+import DoughnutChart from "@/components/dashBoard/DoughnutChart";
+import JoinPbCount from "@/components/dashBoard/JoinPbCount";
+import { useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 import React from "react";
+import { getDashBoardData } from "../apis/dashBoard";
+import { IDashBoard } from "@/types/dashBoard";
 
 function DashBoard() {
-  return <div>DashBoard</div>;
+  const { data, isLoading, isSuccess } = useQuery<unknown, AxiosError, IDashBoard>({
+    queryKey: ["joinList"],
+    queryFn: getDashBoardData,
+    refetchOnWindowFocus: false,
+  });
+
+  return (
+    <div className="flex max-w-[900px] flex-wrap items-center justify-center gap-10">
+      {data?.data && (
+        <>
+          <DoughnutChart dashBoardData={data.data} />
+          <JoinPbCount pbCount={data?.data.pb} />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default DashBoard;
