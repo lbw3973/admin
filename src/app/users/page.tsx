eeb48cce3d0e8881b04341a3f795ad3074f5e381
LeeByoungWook <lbw3973@naver.com>
2023-06-28 04:17:59 +0900

    Feat: 회원 관리 페이지 구현

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+"use client";
+import TableHeader from "@/components/users/TableHeader";
+import { USER_TYPE } from "@/constants/enum";
+import { useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import React, { useEffect, useState } from "react";
+import { Pagination } from "antd";
+import { getUserList } from "../apis/users";
+import TableBody from "@/components/users/TableBody";
+import { IUserInfo } from "@/types/users";
 
 function Users() {
-  return <div>Users</div>;
+  const [totalData, setTotalData] = useState(0);
+  const [nowPage, setNowPage] = useState(0);
+  const [userType, setUserType] = useState(USER_TYPE.PB);
+  const { data, isSuccess } = useQuery<unknown, AxiosError, IUserInfo>([userType, nowPage], () => {
+    return getUserList({ type: userType, page: nowPage });
+  });
+  console.log(data);
+
+  const onChange = (page: number) => {
+    setNowPage(page - 1);
+  };
+
+  useEffect(() => {
+    if (isSuccess && data) {
+      setTotalData(data.totalElements);
+    }
+  }, [isSuccess]);
+
+  return (
+    <>
+      <div
+        className={`mx-auto mb-2.5 flex h-10 ${
+          userType === USER_TYPE.PB ? "w-[853px]" : "w-[1084px]"
+        } gap-4 text-sm font-bold`}
+      >
+        <button
+          onClick={() => setUserType(USER_TYPE.PB)}
+          className={`w-24 rounded-[8px] shadow-md ${
+            userType === USER_TYPE.PB ? "bg-[#334E54] text-white" : "bg-white"
+          }`}
+        >
+          PB
+        </button>
+        <button
+          onClick={() => setUserType(USER_TYPE.USER)}
+          className={`w-24 rounded-[8px] shadow-md ${
+            userType === USER_TYPE.USER ? "bg-[#334E54] text-white" : "bg-white"
+          }`}
+        >
+          투자자
+        </button>
+      </div>
+      <div className={`mx-auto h-[750px] ${userType === USER_TYPE.PB ? "w-[853px]" : "w-[1084px]"} bg-white shadow-md`}>
+        <div className="flex h-[583px] w-full border-b-1 border-[#E0E0E0]">
+          <table className="h-fit table-fixed border-1">
+            <TableHeader userType={userType} />
+            {data?.list.map((item, index) => (
+              <TableBody item={item} index={index + 1} key={index} userType={userType} />
+            ))}
+          </table>
+        </div>
+        <Pagination className="py-2 text-center" onChange={onChange} defaultCurrent={1} total={totalData} />
+      </div>
+    </>
+  );
 }
 
 export default Users;
