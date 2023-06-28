cca57d316af306e8ec0b10638b88e9620a3da9d2
kjun <kjun_all@naver.com>
2023-06-28 19:50:30 +0900

    Feat: FAQ 페이지 API연결, 페이지네이션 연결

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+"use client";
 
-function FAQ() {
-  return <div>FAQ</div>;
+import React, { useEffect, useState } from "react";
+import { getFaqList } from "../apis/faq";
+import { useQuery } from "@tanstack/react-query";
+import { Pagination } from "antd";
+import ListSection from "@/components/common/ListSection";
+import { useRouter } from "next/navigation";
+
+function FaqPage() {
+  const router = useRouter();
+  const [totalData, setTotalData] = useState(0);
+  const [nowPage, setNowPage] = useState(0);
+
+  const { data: faqList, isSuccess } = useQuery(["faq", nowPage], () => {
+    return getFaqList(nowPage);
+  });
+
+  const onChange = (page: number) => {
+    setNowPage(page - 1);
+  };
+
+  useEffect(() => {
+    if (isSuccess && faqList) {
+      setTotalData(faqList.totalElements);
+    }
+  }, [isSuccess, faqList]);
+
+  const createHandler = () => {
+    router.push(`/faq/createFaq`);
+  };
+  if (!faqList) return;
+  return (
+    <div>
+      <ListSection noticeList={faqList.list} nowPage={nowPage} isNotice={false} />
+      <Pagination className="py-2 mt-auto text-center" onChange={onChange} defaultCurrent={1} total={totalData} />
+      <div className="flex justify-end mx-4">
+        <button
+          onClick={createHandler}
+          className="h-[40px] w-[100px] rounded-sm bg-[#425C6F] font-bold text-white hover:bg-primary-normal"
+        >
+          작성하기
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default FAQ;
+export default FaqPage;
