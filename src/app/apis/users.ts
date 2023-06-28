8ebdb27a3a1ca9a2618528dc08914b63e1b9e8ea
LeeByoungWook <lbw3973@naver.com>
2023-06-28 16:21:01 +0900

    Feat: 유저 권한 및 탈퇴 API 구현

--- src/app/apis/users.ts
+++ src/app/apis/users.ts
@@ -4,3 +4,13 @@ export const getUserList = async ({ type, page }: { type: string; page: number }
   const res = await instance.get(`/admin/members?type=${type.toLowerCase()}&page=${page}`);
   return res.data.data;
 };
+
+export const withdrawUser = async ({ type, id }: { type: string; id: number }) => {
+  const res = await instance.delete(`/admin/${type.toLowerCase()}/${id}`);
+  return res.data;
+};
+
+export const setAdminPermission = async ({ id, accept }: { id: number; accept: boolean }) => {
+  const res = await instance.post(`/admin/user/${id}?admin=${accept}`);
+  return res.data;
+};
