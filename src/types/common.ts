429ffd35b0dbe63320b82c8f0147ebd88fdea881
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-28 22:00:19 +0900

    Fix: 타입스크립트 에러 수정

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,5 +1,20 @@
+import { Dispatch, ReactNode, SetStateAction } from "react";
+
 export interface IModalContent {
   content: string;
   confirmText: string;
   confirmFn: () => void;
+}
+export interface ButtonModalProps {
+  modalContents: IModalContents;
+  children?: ReactNode;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+export interface IModalContents {
+  content: string;
+  confirmText: string;
+  cancelText?: string;
+  confirmFn?: () => any;
+  cancelFn?: () => any;
 }
