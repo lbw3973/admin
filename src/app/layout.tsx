cfe3f44eea06c5e4c430aeac9b540920273c9da3
LeeByoungWook <lbw3973@naver.com>
2023-06-29 16:19:19 +0900

    Feat: metadata 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,22 +2,42 @@ import FixedLayout from "@/components/common/FixedLayout";
 import ReactQueryProvider from "./ReactQueryProvider";
 import "./globals.css";
 import { Noto_Sans_KR } from "next/font/google";
+import { Metadata } from "next";
 
 const noto = Noto_Sans_KR({ weight: ["400", "700"], subsets: ["latin"] });
 
-export const metadata = {
-  title: "Admin",
+export const metadata: Metadata = {
+  title: "머니브릿지 ADMIN",
   description: "money-bridge",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ko">
+      <head>
+        <link rel="apple-touch-icon" sizes="57x57" href="/assets/favicons/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/assets/favicons/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/assets/favicons/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/assets/favicons/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/assets/favicons/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/assets/favicons/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/assets/favicons/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/assets/favicons/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/assets/favicons/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/assets/favicons/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicons/favicon-16x16.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/assets/favicons/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className={noto.className}>
         <ReactQueryProvider>
           <FixedLayout>
             <div className="mt-[50px] p-10">
-              <section className="mx-auto h-full w-[1084px] bg-white shadow-md">{children}</section>
+              <section className="h-full w-[1084px] bg-white shadow-md">{children}</section>
             </div>
           </FixedLayout>
         </ReactQueryProvider>
