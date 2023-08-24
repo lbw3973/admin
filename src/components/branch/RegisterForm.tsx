1565e61e96ba18376df3b422da7a5cdfcd168721
kjun <kjun_all@naver.com>
2023-08-24 16:13:47 +0900

    Feat: 다른 지점 등록시 입력값 리셋 적용

--- src/components/branch/RegisterForm.tsx
+++ src/components/branch/RegisterForm.tsx
@@ -1,6 +1,6 @@
 import { registerBranch } from "@/app/apis/branch";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 function RegisterForm({ companyId }: { companyId: number }) {
@@ -28,24 +28,29 @@ function RegisterForm({ companyId }: { companyId: number }) {
       specificAddress: getValues("specificAddress"),
     });
   };
+
+  useEffect(() => {
+    reset();
+  }, [companyId]);
+
   return (
     <div className="h-[228px] p-4">
       <form onSubmit={handleRegister}>
-        <div className="mb-3 flex items-center">
+        <div className="flex items-center mb-3">
           <label className="w-[80px]">지점명</label>
           <input className="form_input" {...register("name")} />
         </div>
-        <div className="mb-3 flex items-center">
+        <div className="flex items-center mb-3">
           <label className="w-[80px]">지점 주소</label>
           <input className="form_input" {...register("address")} />
         </div>
-        <div className="mb-3 flex items-center">
+        <div className="flex items-center mb-3">
           <label className="w-[80px]">상세 주소</label>
           <input className="form_input" {...register("specificAddress")} />
         </div>
         <div className="flex w-full">
           {isError && <p className="text-sm text-status-alert">정확한 도로명/지번 주소로 입력해주세요.</p>}
-          <button className="ml-auto rounded-md bg-primary-normal px-8 py-2 text-white">지점 등록</button>
+          <button className="px-8 py-2 ml-auto text-white rounded-md bg-primary-normal">지점 등록</button>
         </div>
       </form>
     </div>
